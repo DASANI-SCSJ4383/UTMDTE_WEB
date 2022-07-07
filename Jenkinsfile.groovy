cfdef049fe8dabb226631248a7df7e0b53cb7dc8
MUHAMMAD AFIQ BIN MOHD MURAD <76561561+mafiq99@users.noreply.github.com>
2022-07-08 01:43:18 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,30 +1,28 @@
-#!/usr/bin/env groovy
-    node {
-
-      stage('Initialise') {
-        /* Checkout the scripts */
-        checkout scm: [
-                $class: 'GitSCM',
-                userRemoteConfigs: [
-                        [
-                                url: "https://github.com/DASANI-SCSJ4383/UTMDTE_WEB.git",
-                        ]
-                ],
-                branches: [[name: "master"]]
-        ], poll: false
-      }
-
-      stage('Complete any setup steps') {
-		echo "Complete set-up steps"
-      }
-
-      stage('Execute Performance Tests') {
-        dir("${WORKSPACE}/scripts") {
-			bat 'D:/Afiq/Subject/SoftwareConstruction/apache-jmeter-5.5/bin/jmeter.bat -n -t test.jmx -l report.jtl'
+pipeline {
+     agent any
+     stages {
+         stage('Build') {
+             steps {
+                 echo 'Building...'
+             }
+             post {
+                 always {
+                     jiraSendBuildInfo branch: 'master', site: 'dasani.atlassian.net'
+                 }
+             }
+         }
+         stage('Deploy - Production') {
+           when {
+               branch 'master'
+           }
+           steps {
+               echo 'Deploying to Production from master...'
+           }
+           post {
+               always {
+                   jiraSendDeploymentInfo environmentId: '', environmentName: '', environmentType: 'development', issueKeys: [''], serviceIds: [''], site: 'dasani.atlassian.net', state: 'unknown'
+               }
+           }
         }
-      }
-
-      stage('Analyse Results') {
-		echo "Analyse results"
-      }
-    }
+     }
+ }
